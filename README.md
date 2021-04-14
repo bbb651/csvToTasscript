6da2e791e6da8f1d83ff4c4d323d883c251f1bfd
bbb651 <bar.ye651@gmail.com>
2021-04-14 20:24:29 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# csv to tas-script
+
+## Usage
+
+Create a csv file or use the example one (in programs such as Microsoft Excel or LibreOffice Calc, or just a text editor)
+
+Run `py csvToTasscript script1.csv` which creates a tas-script named `script1.txt`
---
+++ csvToTasscript.py
@@ -0,0 +1,41 @@
+import argparse
+import csv
+from pathlib import Path
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('filename')
+	args = parser.parse_args()
+
+	with open(args.filename, newline='') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',')
+		next(reader) # Skip header
+		
+		with open(Path(args.filename).with_suffix('.txt'), 'w') as tasscriptfile:
+
+			for linenumber, row in enumerate(reader, 1):
+
+				buttonnames = [
+					"KEY_A",
+					"KEY_B",
+					"KEY_X",
+					"KEY_Y",
+					"KEY_ZR",
+					"KEY_ZL",
+					"KEY_R",
+					"KEY_L",
+					"KEY_PLUS",
+					"KEY_MINUS",
+					"KEY_DRIGHT",
+					"KEY_DLEFT",
+					"KEY_DUP",
+					"KEY_DDOWN"
+				]
+
+				# Readable python code
+				leftstick = ';'.join([n if n else '0' for n in row[:2]])
+				rightstick = ';'.join([n if n else '0' for n in row[2:4]])
+				buttons = ';'.join([buttonnames[i] for i, x in enumerate(row[4:]) if x])
+
+				tasscriptfile.write(f"{linenumber} {buttons} {leftstick} {rightstick}\n")
